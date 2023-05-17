41250858a7647d8f02110bfbb01d514dfe3290b5
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-18 01:57:03 +0700

    Create Employee.cs

---
+++ Ngu_QLNV/TimNhanVien/QLNV.Core/Entities/Employee.cs
@@ -0,0 +1,36 @@
+﻿
+
+using QLNV.Core.Contracts;
+
+
+namespace TatBlog.Core.Entities;
+
+public class Employee : IEntity
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; }
+
+    public string Gender { get; set; }
+
+    public string Age { get; set; }
+
+    public string Job { get; set; }
+
+    public string TimeOfWork { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public string Phone { get; set; }
+
+    public DateTime Birthday { get; set; }
+
+    public string Resident_ID { get; set; }
+
+    public DateTime JoinedDate { get; set; }
+
+    public string Email { get; set; }
+
+    public string Address { get; set; }
+
+}
