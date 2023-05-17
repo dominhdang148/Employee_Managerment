b23aa488470bd56b7bcecba9bde8abb3ff51d228
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-18 01:57:49 +0700

    Create BlogDbContext.cs

---
+++ Ngu_QLNV/TimNhanVien/QLNV.Data/Contexts/BlogDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Mappings;
+
+namespace TatBlog.Data.Contexts;
+
+public class BlogDbContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=DESKTOP-GJ77F65;Database=QLNV;
+            Trusted_Connection=True;MultipleActiveResultSets=true");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(EmployeeMap).Assembly);
+    }
+
+    public static implicit operator BlogDbContext(QLNV.Services.Blogs.BlogRepository v)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
