662563c449ac50035043f6fa3f8ddc49f18be32d
abcxyzzzzzz <93559767+abcxyzzzzzz@users.noreply.github.com>
2023-05-17 12:52:04 +0700

    Update

--- XoaNhanVien-Tuyen.cs
+++ XoaNhanVien-Tuyen.cs
@@ -1,139 +1,142 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
-class Program
+namespace EmployeeManagement
 {
-	static List<NhanVien> danhSachNhanVien = new List<NhanVien>();
-
-	static void Main(string[] args)
+	public class Employee
 	{
-		// Thêm dữ liệu mẫu vào danh sách nhân viên
-		danhSachNhanVien.Add(new NhanVien("NV001", "Nguyễn Văn A", "Nam", DateTime.Parse("1990-01-15"), "Quản lý", 15000000));
-		danhSachNhanVien.Add(new NhanVien("NV002", "Trần Thị B", "Nữ", DateTime.Parse("1995-05-20"), "Lễ tân", 8000000));
-		danhSachNhanVien.Add(new NhanVien("NV003", "Phạm Văn C", "Nam", DateTime.Parse("1992-11-03"), "Nhân viên phục vụ", 6000000));
-		danhSachNhanVien.Add(new NhanVien("NV004", "Lê Thị D", "Nữ", DateTime.Parse("1998-07-10"), "Nhân viên phục vụ", 6000000));
-		danhSachNhanVien.Add(new NhanVien("NV005", "Hoàng Văn E", "Nam", DateTime.Parse("1993-03-18"), "Quản lý", 12000000));
-		danhSachNhanVien.Add(new NhanVien("NV006", "Trần Thị F", "Nữ", DateTime.Parse("1994-08-22"), "Lễ tân", 7500000));
-		danhSachNhanVien.Add(new NhanVien("NV007", "Võ Văn G", "Nam", DateTime.Parse("1996-12-07"), "Nhân viên phục vụ", 5500000));
-		danhSachNhanVien.Add(new NhanVien("NV008", "Nguyễn Thị H", "Nữ", DateTime.Parse("1999-05-14"), "Nhân viên phục vụ", 5500000));
-		danhSachNhanVien.Add(new NhanVien("NV009", "Trần Văn I", "Nam", DateTime.Parse("1991-09-25"), "Kế toán", 9000000));
-		// Gọi hàm xóa nhân viên với mã nhân viên cần xóa
-		XoaNhanVien("NV002");
-
-		// Gọi hàm xóa nhân viên theo tên
-		XoaNhanVienTheoTen("Nguyễn Văn A");
-
-		// Gọi hàm xóa nhân viên có cùng công việc
-		XoaNhanVienCungCongViec("Nhân viên phục vụ");
-
-		// In danh sách nhân viên sau khi xóa
-		Console.WriteLine("Danh sách nhân viên sau khi xóa:");
-		InDanhSachNhanVien();
-
-		Console.ReadLine();
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Title { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		public string Address { get; set; }
+		public string PhoneNo { get; set; }
+		public string Email { get; set; }
 	}
 
-	static void XoaNhanVien(string maNhanVien)
+	public class EmployeeManager
 	{
-		// Tìm nhân viên có mã tương ứng trong danh sách
-		var nhanVien = danhSachNhanVien.FirstOrDefault(nv => nv.MaNV == maNhanVien);
+		private readonly EmployeeContext _context;
 
-		if (nhanVien != null)
+		public EmployeeManager(EmployeeContext context)
 		{
-			// Xóa nhân viên khỏi danh sách
-			danhSachNhanVien.Remove(nhanVien);
-			Console.WriteLine("Đã xóa nhân viên thành công!");
+			_context = context;
 		}
-		else
-		{
-			Console.WriteLine("Không tìm thấy nhân viên có mã như vậy!");
-		}
-	}
 
-	static void XoaNhanVienTheoTen(string tenNhanVien)
-	{
-		// Tìm nhân viên có tên tương ứng trong danh sách
-		var nhanVien = danhSachNhanVien.FirstOrDefault(nv => nv.TenNV == tenNhanVien);
-
-		if (nhanVien != null)
+		public async Task<bool> AddOrUpdateEmployeeAsync(Employee employee, CancellationToken cancellationToken = default)
 		{
-			// Xóa nhân viên khỏi danh sách
-			danhSachNhanVien.Remove(nhanVien);
-			Console.WriteLine("Đã xóa nhân viên thành công!");
-		}
-		else
-		{
-			Console.WriteLine("Không tìm thấy nhân viên có tên như vậy!");
-		}
-	}
-
-	static void XoaNhanVienCungCongViec(string congViec)
-	{
-		// Tìm những nhân viên có cùng công việc trong danh sách
-		var nhanVienCungCongViec = danhSachNhanVien.Where(nv => nv.ChucVu == congViec).ToList();
-
-		if (nhanVienCungCongViec.Any())
-		{
-			// Xóa từng nhân viên khỏi danh sách
-			foreach (var nhanVien in nhanVienCungCongViec)
+			if (employee.Id > 0)
 			{
-				danhSachNhanVien.Remove(nhanVien);
+				_context.Update(employee);
+			}
+			else
+			{
+				_context.Add(employee);
 			}
 
-			Console.WriteLine("Đã xóa các nhân viên cùng công việc thành công!");
+			var result = await _context.SaveChangesAsync(cancellationToken);
+			return result > 0;
 		}
-		else
-		{
-			Console.WriteLine("Không tìm thấy nhân viên có công việc như vậy!");
-		}
-	}
-	static void XoaNhanVienCungChucVu(string chucVu)
-	{
-		// Tìm những nhân viên có cùng chức vụ trong danh sách
-		var nhanVienCungChucVu = danhSachNhanVien.Where(nv => nv.ChucVu == chucVu).ToList();
 
-		if (nhanVienCungChucVu.Any())
+		public async Task<bool> DeleteEmployeeAsync(int employeeId, CancellationToken cancellationToken = default)
 		{
-			// Xóa từng nhân viên khỏi danh sách
-			foreach (var nhanVien in nhanVienCungChucVu)
+			var employee = await _context.Employees.SingleOrDefaultAsync(e => e.Id == employeeId, cancellationToken);
+			if (employee != null)
 			{
-				danhSachNhanVien.Remove(nhanVien);
+				_context.Employees.Remove(employee);
+				var result = await _context.SaveChangesAsync(cancellationToken);
+				return result > 0;
 			}
 
-			Console.WriteLine("Đã xóa các nhân viên cùng chức vụ thành công!");
+			return false;
 		}
-		else
-		{
-			Console.WriteLine("Không tìm thấy nhân viên có chức vụ như vậy!");
-		}
-	}
-	static void InDanhSachNhanVien()
-	{
-		// In danh sách nhân viên
-		foreach (var nhanVien in danhSachNhanVien)
-		{
-			Console.WriteLine($"{nhanVien.MaNV} - {nhanVien.TenNV} - {nhanVien.GioiTinh} - {nhanVien.NgaySinh.ToShortDateString()} - {nhanVien.ChucVu} - {nhanVien.Luong}");
-		}
-	}
-}
 
-class NhanVien
-	{
-		public string MaNV { get; set; }
-		public string TenNV { get; set; }
-		public string GioiTinh { get; set; }
-		public DateTime NgaySinh { get; set; }
-		public string ChucVu { get; set; }
-		public decimal Luong { get; set; }
+		public async Task<IEnumerable<Employee>> GetEmployeesAsync(CancellationToken cancellationToken = default)
+		{
+			return await _context.Employees.ToListAsync(cancellationToken);
+		}
 
-	public NhanVien(string maNV, string tenNV, string gioiTinh, DateTime ngaySinh, string chucVu, decimal luong)
-	{
-		MaNV = maNV;
-		TenNV = tenNV;
-		GioiTinh = gioiTinh;
-		NgaySinh = ngaySinh;
-		ChucVu = chucVu;
-		Luong = luong;
+		public async Task<IEnumerable<Employee>> GetEmployeesByNameAsync(string name, CancellationToken cancellationToken = default)
+		{
+			return await _context.Employees.Where(e => e.Name == name).ToListAsync(cancellationToken);
+		}
+
+		public async Task<IEnumerable<Employee>> GetEmployeesByTitleAsync(string title, CancellationToken cancellationToken = default)
+		{
+			return await _context.Employees.Where(e => e.Title == title).ToListAsync(cancellationToken);
+		}
 	}
-}
+
+	public class EmployeeContext : DbContext
+	{
+		public DbSet<Employee> Employees { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Employee>()
+				.HasKey(e => e.Id);
+
+			modelBuilder.Entity<Employee>()
+				.Property(e => e.Name)
+				.IsRequired();
+
+			modelBuilder.Entity<Employee>()
+				.Property(e => e.Title)
+				.IsRequired();
+
+			modelBuilder.Entity<Employee>()
+				.Property(e => e.DateOfBirth)
+				.IsRequired();
+
+			modelBuilder.Entity<Employee>()
+				.Property(e => e.Address)
+				.IsRequired();
+
+			modelBuilder.Entity<Employee>()
+				.Property(e => e.PhoneNo)
+				.IsRequired();
+
+			modelBuilder.Entity<Employee>()
+				.Property(e => e.Email)
+				.IsRequired();
+		}
+
+		public string ConnectionString { get; set; }
+	}
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Create a new EmployeeManager instance.
+			var employeeManager = new EmployeeManager(new EmployeeContext());
+
+			// Set the connection string.
+			employeeManager.ConnectionString = @"Data Source=localhost;Initial Catalog=EmployeeManagement;Integrated Security=True";
+
+			// Add a new employee.
+			var employee = new Employee
+			{
+				Name = "John Doe",
+				Title = "Software Engineer",
+				DateOfBirth = DateTime.Now,
+				Address = "123 Main Street",
+				PhoneNo = "123-456-7890",
+				Email = "john.doe@example.com"
+			};
+
+			await employeeManager.AddOrUpdateEmployeeAsync(employee);
+
+			// Get all employees.
+			var employees = await employeeManager.GetEmployeesAsync();
+
+			// Print the employees.
+			foreach (var employee in employees)
+			{
+				Console.WriteLine(employee.Name);
+			}
+
+			Console.ReadKey();
+		}
+	}
