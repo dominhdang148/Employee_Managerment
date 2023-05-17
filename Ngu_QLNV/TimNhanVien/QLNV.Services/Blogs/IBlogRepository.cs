b250471ef7e5f8471b5ed19abdcd184e52185123
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-18 01:58:46 +0700

    Create IBlogRepository.cs

---
+++ Ngu_QLNV/TimNhanVien/QLNV.Services/Blogs/IBlogRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Services.Blogs;
+
+public interface IBlogRepository
+{
+    Task<IEnumerable<Employee>> GetEmployeesByNameAsync(string name, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<Employee>> GetEmployeesByAddressAsync(string address, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<Employee>> GetEmployeesByAgeAsync(string age, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<Employee>> GetEmployeesByJobAsync(string job, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<Employee>> GetEmployeesByTimeOfWorkAsync(string timeOfWork, CancellationToken cancellationToken = default);
+}
+
