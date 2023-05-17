d41274055cc72704e3f15ad851bb7f7de8ef975f
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-18 02:02:16 +0700

    Create Program.cs

---
+++ Ngu_QLNV/TimNhanVien/QLNV.WinApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+
+
+
+var context = new BlogDbContext();
+
+var seeder = new DataSeeder(context);
+
+seeder.Initialize();
+
+var employees = context.Employees.ToList();
+
+Console.WriteLine("{0,-5}{1,-50}{2,10}{2,10}{1,-50}{1,-50}{2,-30}{3,12}{2,-30}{3,12}{2,-30}{1,-50}",
+    "ID", "Full Name", "Gender", "Age", "Job", "TimeOfWork", "Phone", "Birthday", "Resident_ID", "Joined Date", "Email", "Address");
+
+foreach (var employee in employees)
+{
+    Console.WriteLine("{0,-5}{1,-50}{2,10}{2,10}{1,-50}{1,-50}{2,-30}{3,12:MM/dd/yyyy}{2,-30}{3,12:MM/dd/yyyy}{2,-30}{1,-50}",
+        employee.Id, employee.FullName, employee.Gender, employee.Age, employee.Job, employee.TimeOfWork, employee.Phone, employee.Birthday, employee.Resident_ID, employee.JoinedDate, employee.Email, employee.Address);
+}
