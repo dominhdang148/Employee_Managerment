3d58aa1a16f44d1bb8887ce2002056a9f00af235
dominhdang148 <myclone235@gmail.com>
2023-05-19 09:54:31 +0700

    Query Add

---
+++ StaffManage/StaffManage.Services/Manager/StaffRepository.cs
@@ -0,0 +1,17 @@
+﻿using StaffManage.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffManage.Services.Manager
+{
+    class StaffRepository : IStaffRepository
+    {
+        private readonly StaffDbContext _context;
+        public StaffRepository(StaffDbContext context) => _context = context;
+
+        // Code truy vấn ở đây
+    }
+}
