9e3835e47ac29b229a715cb0fef51a413c87f7c5
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-26 11:48:04 +0700

    Update DataSeeder.cs

--- StaffManage/StaffManage.Data/Seeders/DataSeeder.cs
+++ StaffManage/StaffManage.Data/Seeders/DataSeeder.cs
@@ -23,6 +23,7 @@ namespace StaffManage.Data.Seeders
 
             var department = AddDepartment();
             var salary = AddSalary();
+            var employees = AddEmployees();
 
         }
         private IList<Department> AddDepartment()
@@ -55,5 +56,87 @@ namespace StaffManage.Data.Seeders
         //{
 
         //}
-    }
+    
+
+        private IList<EmployeeInfo> AddEmployees()
+        {
+            var employees = new List<EmployeeInfo>()
+        {
+            new()
+            {
+                FullName = "Võ Văn Hoàng",
+                Gender = "Nam",
+                Age = "21",
+                Job = "Lễ tân",
+                TimeOfWork = "Sáng",
+                Phone = "0927849123",
+                Birthday = new DateTime(2002, 01, 21),
+                Resident_ID = "CV01D252",
+                JoinedDate = new DateTime(2022, 10, 21),
+                Email = "vovanhoang@gmail.com",
+                Address = "61 Bà Triệu, Đà Lạt"
+            },
+            new()
+            {
+                FullName = "Trần Đình Công",
+                Gender = "Nam",
+                Age = "21",
+                Job = "Bếp trưởng",
+                TimeOfWork = "Sáng, Chiều",
+                Phone = "0928457295",
+                Birthday = new DateTime(2002, 07, 03),
+                Resident_ID = "CV02D271",
+                JoinedDate = new DateTime(2022, 08, 28),
+                Email = "trandinhcong@gmail.com",
+                Address = "152 Phan Đình Phùng, Đà Lạt"
+            },
+            new()
+            {
+                FullName = "Ngyễn Thị Thủy Tiên",
+                Gender = "Nữ",
+                Age = "20",
+                Job = "Thu ngân",
+                TimeOfWork = "Sáng",
+                Phone = "0972811992",
+                Birthday = new DateTime(2003, 05, 15),
+                Resident_ID = "CV01D252",
+                JoinedDate = new DateTime(2021, 12, 20),
+                Email = "nguyenthithuytien@gmail.com",
+                Address = "1 Trần Khánh Dư, Đà Lạt"
+            },
+            new()
+            {
+                FullName = "Trần Duy Quang",
+                Gender = "Nam",
+                Age = "20",
+                Job = "Bảo vệ",
+                TimeOfWork = "Cả ngày",
+                Phone = "0995038129",
+                Birthday = new DateTime(2003, 06, 30),
+                Resident_ID = "CV04D118",
+                JoinedDate = new DateTime(2023, 11, 11),
+                Email = "tranduyquang@gmail.com",
+                Address = "20 Phạm Ngũ Lão, Đà Lạt"
+            },
+            new()
+            {
+                FullName = "Hải Thị Mỹ Yến",
+                Gender = "Nữ",
+                Age = "21",
+                Job = "Lễ tân",
+                TimeOfWork = "Chiều",
+                Phone = "0986177280",
+                Birthday = new DateTime(2002, 12, 08),
+                Resident_ID = "CV01D252",
+                JoinedDate = new DateTime(2021, 03, 30),
+                Email = "haithimyyen@gmail.com",
+                Address = "102 Trần Phú, Đà Lạt, "
+            }
+        };
+
+            _dbContext.Employees.AddRange(employees);
+            _dbContext.SaveChanges();
+
+            return employees;
+        }
 }
