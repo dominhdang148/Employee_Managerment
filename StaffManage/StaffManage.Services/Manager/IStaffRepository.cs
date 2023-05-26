d9c56b8a7fbe0fec8a7a51c015b6b74bd79b6ca1
vuhuyngu <93934302+vuhuyngu@users.noreply.github.com>
2023-05-26 11:19:06 +0700

    Update IStaffRepository.cs

--- StaffManage/StaffManage.Services/Manager/IStaffRepository.cs
+++ StaffManage/StaffManage.Services/Manager/IStaffRepository.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StaffManage.Core.Entities;
 
 namespace StaffManage.Services.Manager
 {
     interface IStaffRepository
     {
         // Code truy vấn ở đây
+        Task<IEnumerable<Employee>> GetEmployeesByNameAsync(string name, CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<Employee>> GetEmployeesByAddressAsync(string address, CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<Employee>> GetEmployeesByAgeAsync(string age, CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<Employee>> GetEmployeesByJobAsync(string job, CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<Employee>> GetEmployeesByTimeOfWorkAsync(string timeOfWork, CancellationToken cancellationToken = default);
     }
 }
